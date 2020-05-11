41ef0631b5b757c4ba5b198f8f3f44b06e46d422
Morgan Welch <welch5788@live.com>
2020-05-11 12:09:29 -0400

    -Added pages route -Added pages controller -Added pages view -Added pages model -Populate pages view -link to page model objects -view for page model object -Added route for new page -Added view for new page -added action to create page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_051409) do
+ActiveRecord::Schema.define(version: 2020_05_11_161738) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
