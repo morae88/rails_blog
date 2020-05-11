fc08667cdc88444d39006bcc47c077b92299297c
Morgan Welch <welch5788@live.com>
2020-05-11 15:02:32 -0400

    -added edit route -add edit form -created layout for form -added update action -added delete action -using resources shortcut -added filters to page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
   def index
     @pages = Page.all
   end
 
   def show
-    @page = Page.find(params[:id])
   end
 
   def new
@@ -12,9 +13,30 @@ class PagesController < ApplicationController
   end
 
   def create
-    page_params = params.require(:page).permit(:title, :body, :slug)
-    @page = Page.new(page_params)
     @page.save
     redirect_to @page
   end
+
+  def edit
+  end
+
+  def update
+    @page.update(page_params)
+    redirect_to @page
+  end
+
+  def destroy
+    @page.destroy
+    redirect_to pages_path
+  end
+
+private
+  def page_params
+    params.require(:page).permit(:title, :body, :slug)
+  end
+
+  def set_page
+    @page = Page.find(params[:id])
+  end
+
 end
